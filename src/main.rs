139a1dbdd6983726c8c7c473ce23abc249047925
Wvaviator <wvaviator@gmail.com>
2023-10-28 22:59:38 -0400

    set up file arg reading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 mod mp3;
 
+use clap::{command, Arg};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(Arg::new("input").required(true).index(1))
+        .get_matches();
+
+    // Gets a value for INPUT if supplied by the user
+    let input = matches.get_one::<String>("input").unwrap();
+
+    // Convert the string to a Path type
+    let path = Path::new(input);
+
+    // Now you can perform your operations on the path.
+    if path.exists() {
+        println!("The provided file path exists: {}", path.display());
+        // TODO: Implement the main logic of your program here.
+    } else {
+        println!("The provided file path does not exist: {}", path.display());
+    }
+
+    let raw_mp3_bytes = std::fs::read(path).unwrap();
+
+    let mp3 = mp3::MP3::parse(raw_mp3_bytes);
+
+    println!("Frames: {}", mp3.frames.len());
 }
