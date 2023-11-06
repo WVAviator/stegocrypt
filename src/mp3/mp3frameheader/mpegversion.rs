cc3a34d7aae378379ce97b9f0ce4c8cd51f72552
Wvaviator <wvaviator@gmail.com>
2023-11-06 17:00:18 -0600

    quick updates to variable names and code org

--- src/mp3/mp3frameheader/mpegversion.rs
+++ src/mp3/mp3frameheader/mpegversion.rs
@@ -1,5 +1,5 @@
-const MPEG_VERSION_ID: u32 = 0b00000000_00011000_00000000_00000000;
-const MPEG_VERSION_ID_OFFSET: u32 = 19;
+const MPEG_VERSION_ID_MASK: u32 = 0b00000000_00011000_00000000_00000000;
+const MPEG_VERSION_ID_MASK_OFFSET: u32 = 19;
 
 /// The MPEG Version of the file. Most files will be MPEG Version 1.
 #[derive(Copy, Clone)]
@@ -14,7 +14,7 @@ impl MPEGVersion {
     /// Given a 32-bit frame header, parse the MPEG version.
     /// The version ID is extracted from bits 19 and 20 and then mapped to the appropriate MPEGVersion enum.
     pub fn parse(data: u32) -> MPEGVersion {
-        let version_id = (data & MPEG_VERSION_ID) >> MPEG_VERSION_ID_OFFSET;
+        let version_id = (data & MPEG_VERSION_ID_MASK) >> MPEG_VERSION_ID_MASK_OFFSET;
         match version_id {
             0b00 => MPEGVersion::Version2_5,
             0b01 => MPEGVersion::VersionReserved,
@@ -27,7 +27,7 @@ impl MPEGVersion {
     /// Returns a new 32-bit frame header with this version applied.
     /// Bits 19 and 20 from the provided header are overridden, and then the appropriate bits for this version ID applied in the new frame header.
     pub fn apply(&self, header: u32) -> u32 {
-        let result = header & 0b11111111_11100111_11111111_11111111;
+        let result = header & !MPEG_VERSION_ID_MASK;
         result
             | match self {
                 MPEGVersion::Version1 => 0b00000000_00011000_00000000_00000000,
